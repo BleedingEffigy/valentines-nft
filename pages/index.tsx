db999f692d1a820659c2630b0a49e44bad0d36bd
bleedingeffigy <ahern077@gmail.com>
2022-02-15 16:20:32 -0800

    made initial changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,81 +2,70 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div id="bodyy" className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Boring Bananas Company</title>
+        <link rel="icon" href="/images/favicon.jpg" />
+
+        <meta property="og:title" content="Boring Bananas Co." key="ogtitle" />
+        <meta property="og:description" content="Here at Boring Bananas company, we specialise in the world's finest digital bananas. We've put together a team spanning 3 continents, to bring you some of the most ‍NUTRITIOUS and DELICIOUS
+bananas out known to man." key="ogdesc" />
+        <meta property="og:type" content="website" key="ogtype" />
+        <meta property="og:url" content="https://boringbananas.co/" key="ogurl"/>
+        <meta property="og:image" content="https://boringbananas.co/images/Hola.gif" key="ogimage"/>
+        <meta property="og:site_name" content="https://boringbananas.co/" key="ogsitename" />
+
+        <meta name="twitter:card" content="summary_large_image" key="twcard"/>
+        <meta property="twitter:domain" content="boringbananas.co" key="twdomain" />
+        <meta property="twitter:url" content="https://boringbananas.co/" key="twurl" />
+        <meta name="twitter:title" content="Boring Bananas Co." key="twtitle" />
+        <meta name="twitter:description" content="Here at boring Bananas company, we specialise in the world's finest digital bananas. We've put together a team spanning 3 continents, to bring you some of the most ‍NUTRITIOUS and DELICIOUS
+bananas out known to man." key="twdesc" />
+        <meta name="twitter:image" content="https://boringbananas.co/images/Hola.gif" key="twimage" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
+      <div className="min-h-screen py-6 flex flex-col justify-center relative overflow-hidden sm:py-12">
+      <img src="/images/Moon.svg" alt="" className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-none" width="1308" />
+      <div className="absolute inset-0 bg-[url(/img/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
+      <div className="relative px-6 pt-10 pb-8 bg-white shadow-xl ring-1 ring-gray-900/5 sm:max-w-lg sm:mx-auto sm:rounded-lg sm:px-10">
+        <div className="max-w-md mx-auto">
+          <div className="divide-y divide-gray-300/50">
+            <div className="py-8 text-base leading-7 space-y-6 text-gray-600">
+              <h1 className="text-4xl text-pink-500/80 font-extrabold tracking-wider">Valentines White List</h1>
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+              <p>
+                Here you can find a piece of art made by my girlfriend, and minted into a bundle of 
+                erc1155 tokens that live on the Rinkeby testnet.
+              </p>
 
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
+              <p>
+                Holding this token will grant a whitelist for the &nbsp;
+                <a href="https://conedoods.vercel.app/" className="underline">Cone Doods NFT</a> project.
+                Get whitelisted, while learning how to mint a token!
+              </p>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
+              <iframe
+                src="https://cloudflare-ipfs.com/ipfs/QmfDKuuMy6chN9dWichrGzS8amhAyCdw6ttGbRPL5WjgDy/bundledrop.html?contract=0xC4aCd8415ff4f7d9DDb328B4222918f3c62D8b81&chainId=4&tokenId=0"
+                width="600px"
+                height="600px"
+                style={{maxWidth:"100%"}}
+                frameBorder="0"
+              />
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            </div>
+            <div className="pt-8 text-base leading-7 font-semibold bg-pink-500/30 rounded-lg p-2 shadow-md shadow-pink-500/50">
+              <p className="text-gray-900">Want to learn more about Web3?</p>
+              <p>
+                <a href="https://ephemeus.ahernandez.dev/" className="text-pink-500 hover:text-pink-600">Visit our DAO &rarr;</a>
+              </p>
+            </div>
+          </div>
         </div>
-      </main>
+      </div>
+      </div>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="ml-2 h-4" />
-        </a>
-      </footer>
-    </div>
+
+    </div> 
   )
 }
